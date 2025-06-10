fa5c73cbae82d8351c0580b8a1c5d8f3ef6785a9
hetbhalani <bhalanihet2006@gmail.com>
2025-06-10 21:42:51 +0530

    lab-1 code

---
+++ .NET/lab-1/area.cs
@@ -0,0 +1,12 @@
+using System;
+
+class area
+{
+    static void Main(string[] args)
+    {
+        int width = Convert.ToInt32(Console.ReadLine());
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(width * height);
+    }
+}
